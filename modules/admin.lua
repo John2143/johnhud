8d90934be8c44865f071b984192baece4a33db72
John2143658709 <John2143658709@users.noreply.github.com>
2015-02-22 01:35:24 -0500

    csay

--- modules/admin.lua
+++ modules/admin.lua
@@ -2,16 +2,51 @@ this.getPlayers = function(self, ...)
 	return jhud.player:getPlayers(...)
 end
 
-function this:kick(chat, on)
+function this:kick(noban, chat, on)
 	local plys = self:getPlayers(on)
-	_(on)
-
 	for i,v in pairs(plys) do
 		v:kick()
 	end
 end
 
+function this:csay(chat, text)
+	if not jhud.net:isServer() then return end
+	jhud.net("jhud.admin.csay", text)
+end
+function this:csay2(chat, text)
+	if not jhud.net:isServer() then return end
+	jhud.net("jhud.admin.csay2", text)
+end
+
+function this:docsay(text)
+	jhud.dlog("CSAY", text)
+	managers.hud:show_hint{
+		text = text,
+		event = 0,
+		time = 5,
+	}
+end
+function this:docsay2(text)
+	jhud.dlog("CSAY2", text)
+	managers.hud:present_mid_text{ --csay expanding box
+		title = "TODO ADD A WAY TO SET THIS",
+		text = text,
+		icon = "unused",
+		time = 5,
+		event = {}
+	}
+end
+
 function this:__init()
 	jhud.chat:addCommand("kick", function(...) self:kick(true, ...) end)
 	jhud.chat:addCommand("kickb", function(...) self:kick(false, ...) end)
+	jhud.chat:addCommand("csay", function(...) self:csay(...) end)
+	jhud.chat:addCommand("csay2", function(...) self:csay2(...) end)
+
+	jhud.net:hook("jhud.admin.csay", function(data)
+		self:docsay(data)
+	end)
+	jhud.net:hook("jhud.admin.csay2", function(data)
+		self:docsay2(data)
+	end)
 end
