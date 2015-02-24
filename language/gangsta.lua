8537e38b6797c14d319ae0e9521241b71b6b3d9e
tilderain <tilderain@users.noreply.github.com>
2015-02-23 17:51:14 -0800

    Update gangsta.lua

--- language/gangsta.lua
+++ language/gangsta.lua
@@ -17,8 +17,23 @@ LOADLANGUAGE = function() return {
 		trans_error = "lost in translation",
 		affix_error = "-ayyy",
 		cheater = "shit why don't you use pirate perfection",
+		newver = "gimme dat update bro (%s -> %s). do '/update' & get me the hot new code",
+		nonewver = "nigga i aint tell you to update (version %s)",
+		downloading = "git fetch",
+		applying = "git pull",
 	},
 	chat = {
 		unknown = "look it up in the dictionary '%s'"
+		cmdplaying = "homies",
+		noplayer = "tell me the men",
+		solo = "playin with himself man",
+		requiresheist = "go steal some shit",
+		internalerror = "ey wat u doin",
+		needhost = "gotta be the gang leader",
+		kickself = "momentum doesn't work that way",
+		valuechange = "family values '%s' -> '%s'",
+		resetdata = "git reset --hard",
+		writeunpure = "an unpure version file, fresh off the oven (full data)",
+		writepure = "a pure version file, fresh off the oven (version only)",
 	}
 } end
