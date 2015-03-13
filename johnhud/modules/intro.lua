9d7f868072cea9128b32b61781da9e28164c4dbd
tilderain <tilderain@users.noreply.github.com>
2015-03-13 11:28:37 -0700

    my ears are bollixed
    yeyahye run the plan go to the fourthok you floor got thinfection clinic computerbloodquarenteenelevator

--- johnhud/modules/intro.lua
+++ johnhud/modules/intro.lua
@@ -1,6 +1,4 @@
-local IngameWaitingForPlayersState__start_audio_original = IngameWaitingForPlayersState._start_audio
 function IngameWaitingForPlayersState:_start_audio()
-    IngameWaitingForPlayersState__start_audio_original(self)
     if managers.network.game and Network:is_server() then
         managers.network:game():spawn_players()
     end
